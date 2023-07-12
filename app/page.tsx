671a21fc93a75a77db933c5fa1d53676bae03be0
degrecci <hdegrecci@gmail.com>
2023-07-12 20:10:54 -0300

    List games images with aspect ratio and responsivity

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import { axiosInstance } from "@/service/axios";
+import Image from "next/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+
 import { Games } from "./types";
 
 export default async function Home() {
@@ -7,9 +10,23 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {results.map((game) => (
-        <div key={game.id}>{game.name}</div>
-      ))}
+      <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 ">
+        {results.map((game) => (
+          <div key={game.id}>
+            {game.name}
+            <div className="w-full">
+              <AspectRatio ratio={16 / 9}>
+                <Image
+                  src={game.background_image}
+                  fill
+                  className="rounded-md object-cover"
+                  alt="Picture of the author"
+                />
+              </AspectRatio>
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
