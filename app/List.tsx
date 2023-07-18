1fb5d5a913e458170c7767be983025c70f29c8da
Henrique Degrecci <hdegrecci@gmail.com>
2023-07-17 22:25:43 -0300

    Add placeholder for images

--- app/List.tsx
+++ app/List.tsx
@@ -1,6 +1,8 @@
 "use client";
+
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import Image from "next/image";
+import { PlaceholderIcon } from "@/assets/icons/placeholder";
 import { Result } from "./types";
 
 type ListProps = {
@@ -17,7 +19,7 @@ export const ListGames = ({ games }: ListProps) => {
       {games.map((game) => (
         <div key={game.id}>
           {game.name}
-          {game.background_image && (
+          {game.background_image ? (
             <div className="w-full">
               <AspectRatio ratio={16 / 9}>
                 <Image
@@ -29,6 +31,10 @@ export const ListGames = ({ games }: ListProps) => {
                 />
               </AspectRatio>
             </div>
+          ) : (
+            <AspectRatio ratio={16 / 9}>
+              <PlaceholderIcon />
+            </AspectRatio>
           )}
         </div>
       ))}
---
+++ assets/icons/placeholder.tsx
@@ -0,0 +1,22 @@
+export const PlaceholderIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 230 130"
+    width="230"
+    height="130"
+    className="rounded-md object-cover"
+  >
+    <rect width="230" height="130" fill="#cccccc"></rect>
+    <text
+      x="50%"
+      y="50%"
+      dominantBaseline="middle"
+      textAnchor="middle"
+      fontFamily="monospace"
+      fontSize="26px"
+      fill="#FFFFFFFF"
+    >
+      230x130
+    </text>
+  </svg>
+);
