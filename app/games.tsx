fca533d144b69168ecc4ea45c9ffd2473dc94780
Henrique Degrecci <hdegrecci@gmail.com>
2023-07-22 00:56:08 -0300

    Games list with shadcn cards

--- app/games.tsx
+++ app/games.tsx
@@ -1,4 +1,14 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Button } from "@/components/ui/button";
 import { Games } from "./types";
 import Image from "next/image";
 import Link from "next/link";
@@ -20,35 +30,49 @@ export const GamesList = async ({ searchParams }: GamesListProps) => {
   }
 
   return (
-    <div className="grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4">
+    <div className="grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
       {results.map((game) => (
         <div key={game.id}>
-          <Link href={`/game/${game.id}`}>
-            <p className="leading-7 truncate">{game.name}</p>
-          </Link>
-          {game.background_image ? (
-            <div className="w-full">
-              <AspectRatio ratio={16 / 9}>
-                <Image
-                  src={game.background_image}
-                  fill
-                  className="rounded-md object-cover"
-                  alt="Picture of the author"
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                />
-              </AspectRatio>
-            </div>
-          ) : (
-            <AspectRatio ratio={16 / 9}>
-              <Image
-                src={placeholderImage}
-                fill
-                className="rounded-md object-cover"
-                alt="Picture of the author"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
-            </AspectRatio>
-          )}
+          <Card>
+            <CardHeader>
+              <CardTitle>{game.name}</CardTitle>
+              <CardDescription className="truncate">
+                {game.platforms.map(({ platform }) => platform.name).join(", ")}
+              </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              {game.background_image ? (
+                <div className="w-full">
+                  <AspectRatio ratio={16 / 9}>
+                    <Image
+                      src={game.background_image}
+                      fill
+                      className="rounded-md object-cover"
+                      alt="Picture of the author"
+                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    />
+                  </AspectRatio>
+                </div>
+              ) : (
+                <AspectRatio ratio={16 / 9}>
+                  <Image
+                    src={placeholderImage}
+                    fill
+                    className="rounded-md object-cover"
+                    alt="Picture of the author"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  />
+                </AspectRatio>
+              )}
+            </CardContent>
+
+            <CardFooter className="flex justify-end">
+              <Button asChild>
+                <Link href={`/game/${game.id}`}>View</Link>
+              </Button>
+            </CardFooter>
+          </Card>
         </div>
       ))}
     </div>
